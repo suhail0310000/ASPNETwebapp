4bf832f1512ca23ff952848c0ea8f0ba6f6ec637
Suhail0310000 <s341868@oslomet.no>
2021-09-05 14:14:10 +0200

    first commit

---
+++ ukeoppg2/Controllers/KundeController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ukeoppg1.Models;
+
+namespace ukeoppg1.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class KundeController : ControllerBase
+    {
+        private readonly DB _DB;
+
+        public KundeController(DB Db)
+        {
+            _DB = Db;
+        }
+
+        public bool Lagre(Kunde innKunde)
+        {
+            try
+            {
+                _DB.Kunder.Add(innKunde);
+                _DB.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        public List<Kunde> HentAlle()
+        {
+            try
+            {
+                List<Kunde> alleKundene = _DB.Kunder.ToList();
+                return alleKundene;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public bool Slett(int id)
+        {
+            try
+            {
+                Kunde enKunde = _DB.Kunder.Find(id);
+                _DB.Kunder.Remove(enKunde);
+                _DB.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        public Kunde HentEn(int id)
+        {
+            try
+            {
+                Kunde enKunde = _DB.Kunder.Find(id);
+                return enKunde;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public bool Endre(Kunde endreKunde)
+        {
+            try
+            {
+                Kunde enKunde = _DB.Kunder.Find(endreKunde.Id);
+                enKunde.navn = endreKunde.navn;
+                enKunde.adresse = endreKunde.adresse;
+                enKunde.tlf = endreKunde.tlf;
+                enKunde.antall = endreKunde.antall;
+                _DB.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ukeoppg2/Models/Kunde.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ukeoppg1.Models
+{
+    public class Kunde
+    {
+        public int Id { get; set; }
+        public string navn { get; set; }
+        public string adresse { get; set; }
+        public string tlf { get; set; }
+
+        public int antall { get; set; }
+    }
+}
+
